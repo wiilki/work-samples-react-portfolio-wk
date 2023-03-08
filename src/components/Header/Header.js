1884b5836031f44a5234bc83a9d0c0b2f009e31f
Will <willkim0608@gmail.com>
2023-03-07 16:49:03 -0800

    Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
