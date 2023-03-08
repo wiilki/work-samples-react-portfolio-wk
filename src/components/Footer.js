289a4a6aff7638929999f5578ca18a0886a0d780
Will <willkim0608@gmail.com>
2023-03-08 10:48:30 -0800

    Add Footer componenet

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+function Footer() {
+    return (
+        <footer>
+            <div class="container">
+                <h3>Will Kim</h3>
+                <div class="social-links">
+                    <a href="https://www.linkedin.com/in/thewillkim/" target="blank" class="linkedin">LinkedIn</a>
+                </div>
+                <div class="copyright">
+                    &copy; Copyright <strong><span>Will Kim</span></strong>. All Rights Reserved
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
