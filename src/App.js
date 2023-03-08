1b721b6d0009185cb650f9529ac214c8df5b8c24
Will <willkim0608@gmail.com>
2023-03-08 10:52:52 -0800

    Add footer to App.js. Edit title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
