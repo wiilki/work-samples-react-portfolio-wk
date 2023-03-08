f15a5d2cb5e04ea0b5e7ce91b5200c349939b266
Will <willkim0608@gmail.com>
2023-03-08 12:15:27 -0800

    Create separate directory to hold individual project components. Import project to Portfolio.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 import About from "../src/components/About";
+import Portfolio from "../src/components/Portfolio";
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <About></About>
-      <Footer></Footer>
+      <Header />
+      <About />
+      <Portfolio />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Project1 from "../components/Portfolio/Project1";
+
+function Portfolio() {
+    return (
+        <section id="portfolio">
+            <Project1 />
+        </section>
+    );
+}
+
+export default Portfolio;
