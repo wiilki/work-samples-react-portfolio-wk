8d4380fb38dc40ce6a319bdb8159668d98c8284d
Will <willkim0608@gmail.com>
2023-03-09 15:40:41 -0800

    Add main css file. Edit portfolio styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,17 @@ import '../styles/Portfolio.css';
 function Project(props) {
   return (
     <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-      {props.projects.map((project, index) => (
-        <div className="col-lg-4 col-md-6 portfolio-item filter-app" key={project.id || index}>
+      {props.projects.map((project) => (
+        <div className="col-lg-4 col-md-6 portfolio-item filter-app" key={project.id}>
           <div className="portfolio-wrap">
             <img src={project.screenshot} className="img-fluid" alt="screenshot"></img>
             <div className="portfolio-info">
               <h4>{project.title}</h4>
+              <p>{project.type}</p>
               <div className="portfolio-links">
-                <a href={project.repo} title={project.title} data-gallery="portfolioGallery" className="portfolio-lightbox"><i className="bx bx-plus"></i></a>
+                <a href="{project.screenshot}" data-gallery="portfolioGallery"
+                  class="portfolio-lightbox" title={project.title}><i
+                    class="bx bx-plus"></i></a>
               </div>
             </div>
           </div>
