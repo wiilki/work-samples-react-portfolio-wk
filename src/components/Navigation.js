2bdb675555d3a07e3374b01c0a7f1ec07d221593
Will <willkim0608@gmail.com>
2023-03-09 16:40:27 -0800

    Import navigation icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
     <header id="header" className="d-flex flex-column justify-content-center">
       <nav id="navbar" className="navbar nav-menu">
         <ul>
-          <li><a href="#home" className="nav-link scrollto"><i className="bx bx-user"></i> <span>Home</span></a></li>
+          <li><a href="#home" className="nav-link scrollto"><i className="bx bx-home"></i> <span>Home</span></a></li>
           <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
           <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></a></li>
           <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Portfolio</span></a></li>
