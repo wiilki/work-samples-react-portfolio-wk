1f761430b0243fc75e9a7ffe300d22a0a44e8742
Will <willkim0608@gmail.com>
2023-07-06 23:42:12 -0700

    remove form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -51,62 +51,10 @@ function Contact() {
             </div>
           </div>
 
-          <div className="col-lg-8 mt-5 mt-lg-0">
-            <form className="php-email-form">
-              <div className="row">
-                <div className="col-md-6 form-group">
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control"
-                    id="name"
-                    placeholder="Your Name"
-                    required
-                  />
-                </div>
-                <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input
-                    type="email"
-                    className={`form-control ${emailError ? "is-invalid" : ""}`}
-                    name="email"
-                    id="email"
-                    placeholder="Your Email"
-                    required
-                    onBlur={handleBlur}
-                  />
-                  {/* Display error message if email input field has error */}
-                  {emailError && (
-                    <div className="invalid-feedback">
-                      Please include an "@" in the email address.
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div className="form-group mt-3">
-                <textarea
-                  className={`form-control ${messageError ? "is-invalid" : ""}`}
-                  name="message"
-                  rows="5"
-                  placeholder="Message"
-                  required
-                  onBlur={handleBlur}
-                ></textarea>
-                {/* Display error message if message input field has error */}
-                {messageError && (
-                  <div className="invalid-feedback">
-                    Please fill out this field.
-                  </div>
-                )}
-              </div>
-              <div className="text-center">
-                <button type="submit">Send Message</button>
-              </div>
-            </form>
-          </div>
         </div>
       </div>
     </section>
   );
 }
-
+1
 export default Contact;
